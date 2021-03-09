77afb4741b1623170f5aa925a86b1011de109030
mehedihasen <mehedisabuj4@gmail.com>
2021-03-10 04:47:57 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
